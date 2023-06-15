5beee60e102da6d225607d23f3f35624c8eecda2
i-ahumada <ahumadaivan.cpp@gmail.com>
2023-06-15 19:35:58 -0300

    terminal app v1.0.1

--- terminal_downloader.py
+++ terminal_downloader.py
@@ -3,42 +3,80 @@ import os
 os.system('color')
 ans = ""
 def menu():
-    print('Select (by typing de marked words) one of the next options to operate')
-    print('\x1b[3;33;40m'+ '- url' +'\x1b[0m' + ' downloads directly from url')
-    print('\x1b[3;33;40m'+ '- fil' +'\x1b[0m' + ' downloads urls from a file that contains urls')
-    print('\x1b[7;30;41m'+ '- ext' +'\x1b[0m' + '')
+    print('Select (by typing the colored numbers) one of the next options to operate')
+    print('\x1b[3;33;40m'+ '- 1' +'\x1b[0m' + ' downloads directly from url')
+    print('\x1b[3;33;40m'+ '- 2' +'\x1b[0m' + ' downloads urls from a file that contains urls')
+    print('\x1b[3;33;40m'+ '- 3' +'\x1b[0m' + ' downloads first n images by username (up to 6 months from today)')
+    print('\x1b[3;33;40m'+ '- 4' +'\x1b[0m' + ' downloads first n images by username from a file (up to 6 months from today)')
+    print('\x1b[7;30;41m'+ '- 5' +'\x1b[0m' + ' exit')
 menu()
-ans = input('-> ')
+incorrect_input = True
+while incorrect_input:
+    ans = input('-> ').strip()
+    try:
+        ans = int(ans)
+        if not ((ans >= 1) and (ans <= 5)):
+            raise ValueError
+        incorrect_input = False
+    except ValueError:
+        print('\x1b[1;37;41m'+ 'ERROR:' +'\x1b[0m' + ' Input must be an integer between 1 and 5')
+    
 
-while ans != "ext":
-    image_amount = 0
-    if(ans == "url"):
+while ans != 5:
+    downloads_happened = False
+    if(ans == 1):
         url = input('Input url: ')
         try:
             url = url.strip()
             mydownloader.download_from_url(url)
-            image_amount += 1
+            downloads_happened = True
         except mydownloader.instaloader.InstaloaderException as ie:
             print(ie)
         else:
-            print('\x1b[6;30;42m'+ "Success!" +'\x1b[0m')
-    elif(ans == "fil"):
+            print('\x1b[6;30;42m'+ "||||||  POST DOWNLOADED  ||||||" +'\x1b[0m')
+            
+    elif(ans == 2):
         folder_path = os.path.join(os.getcwd(), 'link_folder')
         filename = input('Input file name with extension: ')
         path =  os.path.join(folder_path, filename)
-        if not os.path.exists(path):
-            print('\x1b[1;37;41m'+ 'NO FILE FOUND AS ['+ filename +'] IN ['+ folder_path +']' +'\x1b[0m' + '')
-        else:
+        if os.path.exists(path):
             archivo = open(path,"rt")
+            downloads_counter = 0
             for url in archivo:
-                image_amount += 1
+                downloads_happened = True
+                downloads_counter += 1
                 try:
                     mydownloader.download_from_url(url)
                 except mydownloader.instaloader.InstaloaderException as ie:
                     print(ie)
-            print('\x1b[6;30;42m'+ str(image_amount) + " images downloaded" + '\x1b[0m')
+            print('\x1b[6;30;42m'+ '||||||  ' + str(downloads_counter) + " IMAGES DOWNLOADED  ||||||" + '\x1b[0m')
+        else:
+            print('\x1b[1;37;41m'+ '||||||  NO FILE FOUND AS ['+ filename +'] IN ['+ folder_path +']   ||||||' +'\x1b[0m')
             
-    if image_amount > 0:
+    elif(ans == 3):
+        username = input("Input username: ")
+        n = input("Input number of images from last uploaded: ")
+        downloads_happened = True
+        mydownloader.download_last_n(username, int(n))
+        
+    elif(ans == 4):
+        folder_path = os.path.join(os.getcwd(), 'user_folder')
+        filename = input('Input file name with extension: ')
+        path =  os.path.join(folder_path, filename)
+        if os.path.exists(path):
+            n = input('Input number of images from last uploaded: ')
+            archivo = open(path,"rt")
+            for username in archivo:
+                downloads_happened = True
+                username = username.strip()
+                try:
+                    mydownloader.download_last_n(username, int(n))
+                except mydownloader.instaloader.InstaloaderException as ie:
+                    print(ie)
+        else:
+            print('\x1b[1;37;41m'+ '||||||  NO FILE FOUND AS ['+ filename +'] IN ['+ folder_path +']   ||||||' +'\x1b[0m' + '')
+            
+    if downloads_happened:
         downloads_folder_path = os.path.join(os.getcwd(), 'downloads')
         downloads_folder = os.listdir(downloads_folder_path)
         
@@ -52,7 +90,16 @@ while ans != "ext":
                     os.remove(os.path.join(folder_path, file))
     
     menu()
-    ans = input('-> ')
+    incorrect_input = True
+    while incorrect_input:
+        ans = input('-> ').strip()
+        try:
+            ans = int(ans)
+            if not ((ans >= 1) and (ans <= 5)):
+                raise ValueError
+            incorrect_input = False
+        except ValueError:
+            print('\x1b[1;37;41m'+ 'ERROR: ' +'\x1b[0m' + 'input must be an integer between 1 and 5')
 
 
 
