b72fdc9d061d0d7845421fb2656562c963f867f5
i-ahumada <ahumadaivan.cpp@gmail.com>
2023-06-15 16:47:02 -0300

    download las n

---
+++ downloader_funcs.py
@@ -0,0 +1,48 @@
+import instaloader
+import datetime
+import os
+from dateutil.relativedelta import relativedelta
+from itertools import dropwhile, takewhile
+
+# -- DOWNLOAD ALL FROM USERNAME --
+def download_all(username):
+    L =  instaloader.Instaloader()
+    profile = instaloader.Profile.from_username(L.context, username)
+    for post in profile.get_posts():
+        L.download_post(post, target=profile.username)
+
+# -- DOWNLOAD LAST N POSTS FROM USERNAME --
+def download_last_n(username, n):
+    os.system('color')
+    path = os.getcwd() + '/downloads/@{target}/posts/'
+    L =  instaloader.Instaloader(dirname_pattern=path)
+    profile = instaloader.Profile.from_username(L.context, username)
+    posts = profile.get_posts()
+    
+    SINCE = datetime.datetime.today()
+    UNTIL = SINCE - relativedelta(months=6)
+    
+    last_posts = []
+    try:
+        for post in takewhile(lambda p: p.date > UNTIL, dropwhile(lambda p: p.date > SINCE, posts)):
+            last_posts.append(post)
+        
+        amount_posts = len(last_posts)
+        i = 0
+        for post in last_posts:
+            if(i <= n and i <= amount_posts):
+                L.download_post(post, username)
+            else:
+                break
+            i += 1
+    except Exception:
+        print('\x1b[1;37;41m' + "||||||  ERROR! ("+ Exception +")" + " [" + username + "] ||||||" + '\x1b[0m')
+    else:
+        print('\x1b[6;30;42m' + '||||||  DESCARGADO: [' + username + ': '+ str((n,len(last_posts))[len(last_posts) < n]) +' posts]  ||||||' + '\x1b[0m')
+
+users = ("ndalfonsin", "ivan.ahumada_", "apoptica", "ajshdjklwqjlksahd", "pao_mazza_atelier_de_tortas")
+
+for user in users:
+    download_last_n(user, 10)
+
+
